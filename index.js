de29b7a692c7a2c285c2205342ff1083a7ff6836
TyWalter <walterty89@gmail.com>
2024-04-28 15:14:29 -0500

    Updated JS files to correctly work and create new example logos

--- index.js
+++ index.js
@@ -2,4 +2,39 @@ const {makeLogo} = require("./lib/log");
 const {questions} = require("./lib/questions");
 const {Svg, Circle, Square, Triangle, Text} = require("./lib/shapes");
 
-questions()
+const handleResponses = (resp) => {
+  const svg = new Svg();
+  const text = new Text(resp.txtColor, resp.txt);
+  if(resp.shape === "Circle"){
+    const shape = new Circle(resp.shapeColor);
+    const contents = 
+`${svg.render()}
+  ${shape.render()}
+  ${text.render()}
+</svg>`;
+    generateLogo(resp.shape, contents);
+  } else if(resp.shape === "Square"){
+    const shape = new Square(resp.shapeColor);
+    const contents = 
+`${svg.render()}
+  ${shape.render()}
+  ${text.render()}
+</svg>`;
+    generateLogo(resp.shape, contents);
+  } else if(resp.shape === "Triangle"){
+    const shape = new Triangle(resp.shapeColor);
+    const contents = 
+`${svg.render()}
+  ${shape.render()}
+  ${text.render()}
+</svg>`;
+    generateLogo(resp.shape, contents);
+  }
+}
+
+function generateLogo(svgFile, data){
+makeLogo(`${svgFile}.svg`, data);
+};
+
+
+questions(handleResponses);
